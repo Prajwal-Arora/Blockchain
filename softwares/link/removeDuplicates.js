469192a1f84c523b1778b1d83e8e8131bfbadc2b
Prajwal 아로라 <prajwalarora4@gmail.com>
2022-05-09 14:22:17 +0530

    Update removeDuplicates.js

--- softwares/link/removeDuplicates.js
+++ softwares/link/removeDuplicates.js
@@ -7,17 +7,3 @@ module.exports.getUnique = (arr) => {
     }
     return uniqueArr;
 }
-
-// getTransactedAddresses("0xaa25aa7a19f9c426e07dee59b12f944f4d9f1dd3")
-// .then((a) => {
-    
-//     var counter = 0;
-//     var i = setInterval(() => {
-//         getTransactedAddresses(a[counter])
-//         .then(b => console.log(b))
-//         counter++;
-//         if(counter === a.length - 1) {
-//             clearInterval(i);
-//         }
-//     }, 1000);
-// })
